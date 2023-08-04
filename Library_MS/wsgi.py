30d4f5a52c85fc446b76ab613aac408e754e5e75
Victor Abuka <vickerdent@gmail.com>
2023-08-04 06:34:15 +0100

    Make ready for production with wsgi

--- Library_MS/wsgi.py
+++ Library_MS/wsgi.py
@@ -10,11 +10,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR)
+project_folder = os.path.expanduser('~/library_ms-web')
+load_dotenv(os.path.join(project_folder, '.env'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Library_MS.settings')
 
